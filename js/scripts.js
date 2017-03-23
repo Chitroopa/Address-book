5d76313bd58de53265a00e3e347a9bcd1388a44d
Chitroopa M <chitroopa72193@gmail.com>
2017-03-22 22:10:05 -0700

    Add dynamic logic to display selected items from the select box

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,26 @@
 $(document).ready(function() {
+  $('#dropdown').click(function(event)
+  {
+      $('ul').hide();
+      var newItem = '#' + $('#dropdown').val();
+      $(newItem).show();
+  });
+
   $("#addC").submit(function(event) {
     event.preventDefault();
+    var count = parseInt($(".counter").text())+1;
+
     var nameInput = $("#name").val();
     var eAddInput = $("#eAddress").val();
     var phoneInput = parseInt($("#phone").val());
-    var optionText = "<option>"+nameInput+"</option>"
+
+    var optionText = "<option>"+nameInput+count+"</option>";
+    var ulText = "<ul id="+nameInput+count+"><li>Name:"+nameInput+"</li><li>Email:"
+                  +eAddInput+"</li><li>Phone:"+phoneInput+"</li>";
 
     $("#dropdown").append(optionText);
-
-
+    $(".disp").append(ulText);
+    $(".counter").text(count);
+    $('ul').hide();
   });
 });
